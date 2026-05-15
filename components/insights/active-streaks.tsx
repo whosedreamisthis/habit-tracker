4f1ec56518db0e3fba322bc225ca6abed773dd1c
Dana Sharon <whosedreamisthis@gmail.com>
2026-05-15 09:51:19 -0700

    Update ActiveStreaks component to include streak summary counter in header

--- components/insights/active-streaks.tsx
+++ components/insights/active-streaks.tsx
@@ -5,7 +5,10 @@ import ActiveStreakCard from "@/components/insights/active-streak-card";
 const ActiveStreaks = ({ habits }: { habits: Habit[] }) => {
   return (
     <div className="bg-white rounded-lg` shadow-md p-4 mt-5">
-      <p className="text-sm font-bold text-slate-800 mb-5">Active streaks</p>
+      <div className="flex items-center justify-between mb-5">
+        <p className="text-sm font-bold text-slate-800 ">Active streaks</p>
+        <p className="text-xs text-slate-500">5 of 7</p>
+      </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
         {habits.map((habit) => (
           <ActiveStreakCard key={habit._id} {...habit} activeStreak={13} />
