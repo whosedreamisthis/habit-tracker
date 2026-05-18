4208794d218b6f0fc2a546042b5cba8e53e9e134
Dana Sharon <whosedreamisthis@gmail.com>
2026-05-17 18:52:26 -0700

    percentage and completed habits of total habits no longer hardcoded.

--- components/dashboard/today-habits/today-habit-header.tsx
+++ components/dashboard/today-habits/today-habit-header.tsx
@@ -5,14 +5,19 @@ import { Habit } from "@/lib/types";
 const TodayHabitHeader = ({ habits }: { habits: Habit[] }) => {
   const completedHabits = habits.filter((h) => h.isCompletedToday).length;
   const totalHabits = habits.length;
-  const percentage = (completedHabits / totalHabits) * 100;
+  let percentage;
+  if (totalHabits === 0) {
+    percentage = 0;
+  } else {
+    percentage = (completedHabits / totalHabits) * 100;
+  }
   const formattedPercentage = Number(percentage.toFixed(0));
   return (
     <div className="flex items-center justify-between">
       <div>
         <p className="text-md dark:text-stone-100">Today&apos;s habits</p>
         <p className="text-sm text-muted-foreground dark:text-stone-400">
-          4 of 7 complete
+          {completedHabits} of {totalHabits} complete
         </p>
       </div>
       <CircularProgress
